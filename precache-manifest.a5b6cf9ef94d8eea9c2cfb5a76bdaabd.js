9cfe06b54f9f0f64d325df02e9e47edb1d4c32fc
sasha <korbutowski@gmail.com>
2019-11-02 12:09:43 +0300

    Updates

---
+++ precache-manifest.a5b6cf9ef94d8eea9c2cfb5a76bdaabd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e1a161bd02b8eaf4268326fb5b1488",
+    "url": "/React_SN/index.html"
+  },
+  {
+    "revision": "624a6da4d227b3d553da",
+    "url": "/React_SN/static/css/main.eefc9b61.chunk.css"
+  },
+  {
+    "revision": "1f9d2727609f41c83ab0",
+    "url": "/React_SN/static/js/2.d9ef821d.chunk.js"
+  },
+  {
+    "revision": "624a6da4d227b3d553da",
+    "url": "/React_SN/static/js/main.12f401c5.chunk.js"
+  },
+  {
+    "revision": "9b9e67a6866bf854cbf9",
+    "url": "/React_SN/static/js/runtime~main.9f41042c.js"
+  }
+]);
