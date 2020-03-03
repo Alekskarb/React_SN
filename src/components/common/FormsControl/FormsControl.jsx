030f8b3771cc1604c2facf1f9b52c50ecca18ac3
Alekskarb <korbutowski@gmail.com>
2020-03-03 10:47:02 +0300

    validation class style

--- src/components/common/FormsControl/FormsControl.jsx
+++ src/components/common/FormsControl/FormsControl.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import s from './FormsControl.module.css';
 
 export const TextArea = ({input, meta, ...props}) => {
+    const hasError = meta.touched && meta.error;
     return (
-        <div>
-            <textarea {...input} {...props}/>
+        <div className={s.formControl + ' ' + (hasError ? s.error: '')}>
+            <div>
+                <textarea {...input} {...props}/>
+            </div>
+            {hasError && <span> 'some error' </span>}
         </div>
     )
 };
