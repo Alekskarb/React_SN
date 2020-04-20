1ec6e24c6b3baacc6c8d1667335e2e8db46ade1b
Alekskarb <korbutowski@gmail.com>
2020-04-20 20:47:37 +0300

    'memo'

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,7 +4,7 @@ import Post from './Posts/Post';
 import {Field, reduxForm} from "redux-form";
 import {maxLengthTC, requiredFields} from "../../utilites/validators/validators";
 import {TextArea} from "../../common/FormsControl/FormsControl";
-import store from "../../../Redux/redux-store";
+
 
 const maxLength10 = maxLengthTC(10);
 
@@ -23,36 +23,37 @@ const PostForm = (props) => {
 };
 const ReduxPostForm = reduxForm({form: 'reduxPostForm'})(PostForm);
 
-class MyPosts extends React.Component {
+const MyPosts = (props) => {
+    //checks extra renders
+//     componentDidMount() {
+//         setTimeout(() => {
+//             this.setState({a:12})
+//         }, 1000);
+//     }
+//use 1 PureComponent instead of React.Component // class Component
+//use 2 shouldComponentUpdate = React.Component // class Component
+//use 2 React.Memo with functional Component //
 
-    // componentDidMount() {
-    //     setTimeout(() => {
-    //         this.setState({a:12})
-    //     }, 1000);
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextProps !== this.props || nextState !== this.state;
     // }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps !== this.props || nextState !== this.state;
-    }
-
-    render() {
-        console.log('RENDER');
-        let postsElements = this.props.myPost.map((p) => <Post message={p.message} key={p.id}
-                                                               likesCount={p.likesCount}/>);
-        let addPost = (e) => {
-            this.props.onAddPost(e.postFormMess);
-        };
-        return <div className={s.postsBlock}>
-            <h3> My posts </h3>
-            <div>
-                <ReduxPostForm onSubmit={addPost}/>
-                {/*<textarea onChange={onPostChange} value={newPostTexts}/>*/}
-            </div>
-            <div className={s.posts}>
-                {postsElements}
-            </div>
+    console.log('RENDER');
+    let postsElements = props.myPost.map((p) => <Post message={p.message} key={p.id}
+                                                      likesCount={p.likesCount}/>);
+    let addPost = (e) => {
+        props.onAddPost(e.postFormMess);
+    };
+    return <div className={s.postsBlock}>
+        <h3> My posts </h3>
+        <div>
+            <ReduxPostForm onSubmit={addPost}/>
+            {/*<textarea onChange={onPostChange} value={newPostTexts}/>*/}
         </div>
-    }
+        <div className={s.posts}>
+            {postsElements}
+        </div>
+    </div>
 }
 
 export default MyPosts;
