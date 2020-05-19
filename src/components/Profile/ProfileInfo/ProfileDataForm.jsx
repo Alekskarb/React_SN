831d48068c9ca9a0c9566df4ea71c8f542f6602f
Alekskarb <korbutowski@gmail.com>
2020-05-19 17:40:59 +0300

    'updateProfile'

--- src/components/Profile/ProfileInfo/ProfileDataForm.jsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm.jsx
@@ -1,9 +1,24 @@
 import React, {useState} from 'react';
-import s from './ProfileInfo.module.css';
+import {buildField, InputArea} from "../../common/FormsControl/FormsControl";
+// import s from './ProfileInfo.module.css';
 
-const ProfileDataForm = () => {
-    return <div>
-        FORM
-    </div>
+const ProfileDataForm = ({profile}) => {
+    return <form>
+        <div><button onClick={()=>{''}}> save profile</button></div>
+        <div>
+            <b> Full Name: </b> {buildField('Full Name', 'fullName', [], InputArea)}
+        </div>
+        <div><b>Looking for a job: </b> {profile.lookingForAJob ? 'yes' : 'no'} </div>
+        {profile.lookingForAJob &&
+        <div>
+            <b> My professional skills: </b> {profile.lookingForAJobDescription} </div>
+        }
+        <div><b>About Me: </b>{profile.aboutMe} </div>
+        <div>
+        {/*    <b> CONTACTS: </b> {Object.keys(profile.contacts).map(key => {*/}
+        {/*    return <Contacts key={key} title={key} value={profile.contacts[key]}/>*/}
+        {/*})}*/}
+        </div>
+    </form>
 };
 export default ProfileDataForm;
