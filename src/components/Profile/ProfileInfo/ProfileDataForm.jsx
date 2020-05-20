eb61552cbf950f22f8311e38b064161d5a4ddf03
Alekskarb <korbutowski@gmail.com>
2020-05-20 10:26:11 +0300

    'updateProfile'

--- src/components/Profile/ProfileInfo/ProfileDataForm.jsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm.jsx
@@ -1,19 +1,25 @@
 import React, {useState} from 'react';
-import {buildField, InputArea} from "../../common/FormsControl/FormsControl";
+import {buildField, InputArea, TextArea} from "../../common/FormsControl/FormsControl";
+import {handleSubmit, reduxForm} from "redux-form";
 // import s from './ProfileInfo.module.css';
 
-const ProfileDataForm = ({profile}) => {
-    return <form>
-        <div><button onClick={()=>{''}}> save profile</button></div>
+const ProfileDataForm = ({profile, handleSubmit}) => {
+    return <form onSubmit={handleSubmit}>
+        <div><button> save profile </button></div>
         <div>
             <b> Full Name: </b> {buildField('Full Name', 'fullName', [], InputArea)}
         </div>
-        <div><b>Looking for a job: </b> {profile.lookingForAJob ? 'yes' : 'no'} </div>
-        {profile.lookingForAJob &&
         <div>
-            <b> My professional skills: </b> {profile.lookingForAJobDescription} </div>
-        }
+            <b>Looking for a job: </b> {profile.lookingForAJob ? 'yes' : 'no'}
+            {buildField('', 'LookingForAJob', [], InputArea, {type: 'checkbox'})}
+        </div>
+        <div>
+            <b> My professional skills: </b>
+            {buildField('My professional skills', 'lookingForAJobDescription', [], TextArea)}
+        </div>
+
         <div><b>About Me: </b>{profile.aboutMe} </div>
+        {buildField('About Me', 'aboutMe', [], TextArea)}
         <div>
         {/*    <b> CONTACTS: </b> {Object.keys(profile.contacts).map(key => {*/}
         {/*    return <Contacts key={key} title={key} value={profile.contacts[key]}/>*/}
@@ -21,4 +27,7 @@ const ProfileDataForm = ({profile}) => {
         </div>
     </form>
 };
-export default ProfileDataForm;
+
+const ProfileReduxDataForm = reduxForm({form: 'edit profile'})(ProfileDataForm);
+
+export default ProfileReduxDataForm;
